acaf9b63dc2e99d761e8d4d8feeff96ca8db649b
thmsmarque <thmsmarque@gmail.com>
2023-10-17 17:00:43 +0200

    Gestion des sprites

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -1,19 +1,34 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
 
 #include "sprite.h"
 
 
-Sprite::Sprite(SDL_Renderer* renderer, const char *pathfile, int w, int l): width(w), length(l)//constructeur
+Sprite::Sprite(SDL_Renderer* renderer, const char *pathfile, int w, int l, int format): width(w), length(l)//constructeur
 {
-	SDL_Surface *surface = SDL_LoadBMP(pathfile);
-    if(!surface)
+    if(format == 0)
     {
-        printf("SDL_LoadBMP failed");
-    }
+    	SDL_Surface *surface = SDL_LoadBMP(pathfile);
+        if(!surface)
+        {
+            printf("SDL_LoadBMP failed");
+        }
 	
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+    }else
+    {
+         SDL_Surface* surface = IMG_Load(pathfile);
+        if (surface == NULL) 
+        {
+            // Gérer l'erreur de chargement de l'image
+            fprintf(stderr, "Erreur de chargement de l'image : %s\n", IMG_GetError());
+        }
+        // Créer une texture à partir de la surface
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);  // Libérer la surface, car la texture la copie
+    }
 }
 
 Sprite::Sprite()
@@ -27,17 +42,30 @@ Sprite::~Sprite()
     //Destruction
 }
 
-void Sprite::setSprite(SDL_Renderer* renderer, const char *pathfile, int w, int l)
+void Sprite::setSprite(SDL_Renderer* renderer, const char *pathfile, int w, int l, int format)
 {
     width = w; length = l;
-    SDL_Surface *surface = SDL_LoadBMP(pathfile);
-    if(!surface)
+    if(format == 0)
     {
-        printf("SDL_LoadBMP failed");
-    }
+        
+        SDL_Surface *surface = SDL_LoadBMP(pathfile);
+        if(!surface)
+        {
+            printf("SDL_LoadBMP failed");
+        }
     
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+    }else
+    {
+        SDL_Surface* surface = IMG_Load(pathfile);
+        if (surface == NULL) 
+        {
+            fprintf(stderr, "Erreur de chargement de l'image : %s\n", IMG_GetError());
+        }
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+    }
 }
 
 
@@ -59,4 +87,13 @@ void Sprite::displayText(SDL_Renderer* renderer, SDL_Rect dst)
 {
     SDL_Rect src = this->getRect();
     SDL_RenderCopy(renderer,this->getTexture(),&src,&dst);
+}
+
+SDL_Point Sprite::GetRectCenter() 
+{
+    SDL_Point center;
+    SDL_Rect rect = this->getRect();
+    center.x = rect.x + rect.w / 2;
+    center.y = rect.y + rect.h / 2;
+    return center;
 }
--- src/sprite.h
+++ src/sprite.h
@@ -11,14 +11,15 @@
 
 class Sprite{
 public:
-	Sprite(SDL_Renderer* renderer, const char *pathfile, int w, int l);
+	Sprite(SDL_Renderer* renderer, const char *pathfile, int w, int l, int format);//format = 0 si .bmp ou =1 si .png
 	Sprite();
 	~Sprite();//Destructeur
-	void setSprite(SDL_Renderer* renderer, const char *pathfile, int w, int l);
+	void setSprite(SDL_Renderer* renderer, const char *pathfile, int w, int l,int format);
     SDL_Rect getRect();
     SDL_Rect getRectPos(int x, int y);
     SDL_Texture* getTexture();
     void displayText(SDL_Renderer* renderer, SDL_Rect dst);
+    SDL_Point GetRectCenter();
 
 private:
 	SDL_Texture* texture = NULL;
